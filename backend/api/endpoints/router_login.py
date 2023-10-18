ed762522eda6c0b84d1c2238cc97abaa5a8dbba4
sailor0913 <sailor0913@163.com>
2023-10-19 01:06:46 +0800

    优化LoginView

--- backend/api/endpoints/router_login.py
+++ backend/api/endpoints/router_login.py
@@ -1,5 +1,5 @@
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
 from db.database import get_db
 import jwt
@@ -13,7 +13,7 @@ router_login = APIRouter()
 
 
 @router_login.post("/get_token")
-def login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+def login(response: Response, db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
     user = db.query(User).filter(User.username == form_data.username).first()
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(status_code=400, detail="账号或密码错误")
@@ -23,5 +23,11 @@ def login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = 
         "password": user.hashed_password
     }
     token = create_jwt_token(data)
+    response.set_cookie(key="token", value=token, httponly=True, samesite="strict")
     return {"access_token": token, "token_type": "bearer"}
 
+
+@router_login.post("/logout")
+def logout(response: Response):
+    response.delete_cookie(key="token")
+    return {"detail": "Successfully logged out"}
