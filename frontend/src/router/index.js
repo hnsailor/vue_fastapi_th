35791ebb93f0c4af3811c3acc4cd0fc2328697a2
sailor0913 <sailor0913@163.com>
2023-10-18 21:46:30 +0800

    修改前端框架为最基本可以跳转login和index

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory (import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: LoginView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/index',
+      name: 'index',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/HomeView.vue')
     }
   ]
 })
